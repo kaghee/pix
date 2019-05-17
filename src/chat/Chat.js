8b4a27ac9eee4382f07b2b5f08e48ee6b6a3ff66
Kassai Ágnes <kass.agnes@gmail.com>
2019-05-17 15:41:03 +0200

    remove unnecessary code

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -3,28 +3,11 @@ import MessageList from './MessageList';
 import SendMessageForm from './SendMessageForm';
 
 export default class Chat extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      canGuess: true,
-    };
-  }
-
-  componentDidMount() {
-    this.props.socket.on('endRound', () => {
-      this.setState({
-        canGuess: true,
-      });
-    });
-  }
-
   handleGuess = (guess) => {
     this.props.socket.emit('incomingGuess', this.props.name, guess);
   }
 
   render() {
-    console.log(this.props.disabled);
     return (
       <div className="chat">
         <MessageList messages={this.props.messages} />
